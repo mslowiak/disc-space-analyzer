7ec107351c1d5325e286a2db906422cadcacb37d
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-01-12 20:01:25 +0100

    Add elements action events

--- app/view/advanced_search_widget.py
+++ app/view/advanced_search_widget.py
@@ -6,6 +6,7 @@ class AdvancedSearchWidget(QWidget):
     """
     Widget class with advanced search view.
     """
+
     def __init__(self, parent=None):
         """"
         Init method.
@@ -13,3 +14,73 @@ class AdvancedSearchWidget(QWidget):
         """
         super(AdvancedSearchWidget, self).__init__(parent)
         uic.loadUi('ui/advanced_search_component.ui', self)
+        self.handle_buttons(parent)
+        self.handle_checkboxes()
+
+    def handle_buttons(self, parent):
+        """
+        Handle all buttons action events.
+        :param parent: - parent window
+        """
+        self.homeButton.clicked.connect(parent.start_scan_view)
+        self.scanButton.clicked.connect(self.extract_data_from_fields)
+
+    def extract_data_from_fields(self):
+        """
+        Extract data from elements in component
+        """
+        creation_date_from = self.createdFromDate.dateTime()\
+            .toString(self.createdFromDate.displayFormat())
+        creation_date_to = self.createdToDate.dateTime()\
+            .toString(self.createdToDate.displayFormat())
+        file_size_from = self.fileSizeFromSpinBox.value()
+        file_size_to = self.fileSizeToSpinBox.value()
+        parent_path = self.parentPathTextInput.toPlainText()
+        extensions = self.extensionsTextInput.toPlainText()
+
+        print("date from: " + str(creation_date_from))
+        print("date to: " + str(creation_date_to))
+        print("file size from: " + str(file_size_from))
+        print("file size to: " + str(file_size_to))
+        print("parent path: " + str(parent_path))
+        print("extensions: " + str(extensions))
+
+    def handle_checkboxes(self):
+        """
+        Handle all checkboxes state change.
+        """
+        self.parentPathCheckbox.stateChanged.connect(
+            lambda: self.state_changed(
+                self.parentPathCheckbox,
+                self.parentPathTextInput)
+        )
+        self.extensionsCheckbox.stateChanged.connect(
+            lambda: self.state_changed(
+                self.extensionsCheckbox,
+                self.extensionsTextInput)
+        )
+        self.fileSizeCheckbox.stateChanged.connect(
+            lambda: self.state_changed(
+                self.fileSizeCheckbox,
+                self.fileSizeFromSpinBox,
+                self.fileSizeTopSpinBox
+            )
+        )
+        self.creationDateCheckbox.stateChanged.connect(
+            lambda: self.state_changed(
+                self.creationDateCheckbox,
+                self.createdFromDate,
+                self.createdToDate
+            )
+        )
+
+    @staticmethod
+    def state_changed(checkbox, *elements):
+        """
+        Handle checkbox tick connected with fields given in :param elements:.
+        :param checkbox: - checkbox for which state is checked
+        :param elements: - elements managed by checkbox
+        """
+        status = checkbox.isChecked()
+        for elem in list(elements):
+            elem.setDisabled(status)
