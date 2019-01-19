5f6648e89ce8f55fc94f5fba70e75786b8b6c43a
Marcin Słowiak <marcin.slowiak.007@gmail.com>
2019-01-19 16:28:14 +0100

    Add mock data to top size files and dirs view

--- app/view/results_default_search_widget.py
+++ app/view/results_default_search_widget.py
@@ -1,4 +1,6 @@
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QWidget
 
 
@@ -13,3 +15,71 @@ class ResultsDefaultSearchWidget(QWidget):
         """
         super(ResultsDefaultSearchWidget, self).__init__(parent)
         uic.loadUi('ui/results_default_search_component.ui', self)
+
+        top_size_files_header = ['File', 'Location', 'Size']
+        top_size_dirs_header = ['Directory', 'Location', 'Size']
+
+        mock_data_files = [
+            ['plik1.txt', 'D:folder\\ukryte', 142],
+            ['plik2.txt', 'D:folder\\ukryte\\A', 1420]
+        ]
+
+        mock_data_dirs = [
+            ['STUDIA', 'D:folder\\nauka', 3256],
+            ['python', 'D:folder\\nauka\\projekty', 1233]
+        ]
+
+        self.initialize_results_tree()
+        self.initialize_top_ten_size_files(top_size_files_header, mock_data_files)
+        self.initialize_top_ten_size_dirs(top_size_dirs_header, mock_data_dirs)
+
+    def initialize_results_tree(self):
+        print(self.topTenDirsTableView.horizontalHeader().size().width())
+        print(self.topTenFilesTableView.horizontalHeader().size().width())
+        pass
+
+    def initialize_top_ten_size_dirs(self, header_labels, data):
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(header_labels)
+
+        self.topTenDirsTableView.setModel(model)
+
+        header = self.topTenDirsTableView.horizontalHeader()
+        table_width = header.size().width()
+        columns_width = [table_width / 9 * 2, table_width / 9 * 5, table_width / 9 * 2]
+        for idx, value in enumerate(columns_width):
+            self.topTenDirsTableView.setColumnWidth(idx, value)
+
+        for items in data:
+            row = []
+            for item in items:
+                item = QStandardItem(str(item))
+                item.setEditable(False)
+                row.append(item)
+            model.appendRow(row)
+
+        self.topTenDirsTableView.sortByColumn(2, Qt.DescendingOrder)
+        self.topTenDirsTableView.verticalHeader().setVisible(False)
+
+    def initialize_top_ten_size_files(self, header_labels, data):
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(header_labels)
+
+        self.topTenFilesTableView.setModel(model)
+
+        header = self.topTenFilesTableView.horizontalHeader()
+        table_width = header.size().width()
+        columns_width = [table_width / 9 * 2, table_width / 9 * 5, table_width / 9 * 2]
+        for idx, value in enumerate(columns_width):
+            self.topTenFilesTableView.setColumnWidth(idx, value)
+
+        for items in data:
+            row = []
+            for item in items:
+                item = QStandardItem(str(item))
+                item.setEditable(False)
+                row.append(item)
+            model.appendRow(row)
+
+        self.topTenFilesTableView.sortByColumn(2, Qt.DescendingOrder)
+        self.topTenFilesTableView.verticalHeader().setVisible(False)
