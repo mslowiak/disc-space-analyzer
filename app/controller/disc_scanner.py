0953e75bbd5aec7ff0db397afa552517a0c07884
Konrad <konrad.korus0@gmail.com>
2019-01-29 01:33:04 +0100

    Results default search

--- app/controller/disc_scanner.py
+++ app/controller/disc_scanner.py
@@ -1,29 +1,39 @@
 import glob
 import os
+from collections import namedtuple, deque
 
-from model.file_tree import Tree, Node
+from PyQt5.QtGui import QStandardItem
+
+from model.file_tree import Node
+
+File = namedtuple('File', ['name', 'size', 'pardir'])
 
 
-def build_file_tree(start_path):
-    root = Node(start_path, os.path.isdir(start_path))
-    tree = Tree(root)
+def build_file_tree(start_path=os.path.expanduser('~')):
+    root = Node(start_path, os.path.isdir(start_path), level=0, parent=None)
     queue = [root]
+    ret = []
     while queue:
-        node = queue.pop(0)
-        node.children = [Node(os.path.abspath(os.path.join(node.path, path)),
-                              os.path.isdir(os.path.abspath(os.path.join(node.path, path))))
-                         for path in os.listdir(node.path)]
-        queue.extend(node.get_children_dirs())
-    return tree
+        try:
+            node = queue.pop(0)
+            ret.append(node)
+            if node.is_dir:
+                node.children = [Node(os.path.abspath(os.path.join(node.path, path)),
+                                      os.path.isdir(os.path.abspath(os.path.join(node.path, path))), level=node.level + 1, parent=node)
+                                 for path in os.listdir(node.path)]
+                queue.extend(node.children)
+        except (PermissionError, FileNotFoundError):
+            continue
+    return ret
 
 
 def update_biggest(n, biggest, files, root):
     for f in files:
         f = os.path.abspath(os.path.join(root, f))
         f_size = os.path.getsize(f)
-        biggest.append((f, f_size))
+        biggest.append(File(f, f_size, os.path.abspath(os.path.join(f, os.pardir))))
         if len(biggest) > n:
-            min_ = min(biggest, key=lambda tup: tup[1])
+            min_ = min(biggest, key=lambda p: p.size)
             biggest.remove(min_)
     return biggest
 
@@ -33,17 +43,21 @@ def get_n_biggest(start_dir=os.path.expanduser('~'), n=10, consider_files=True, 
     biggest = []
     if recursive:
         for root, dirs, files in os.walk(os.path.abspath(start_dir)):
-            if consider_directories:
-                update_biggest(n, biggest, dirs, root)
-            if consider_files:
-                update_biggest(n, biggest, files, root)
+            try:
+                if consider_directories:
+                    biggest = update_biggest(n, biggest, dirs, root)
+                if consider_files:
+                    biggest = update_biggest(n, biggest, files, root)
+            except (FileNotFoundError, OSError):
+                continue
     else:
-        biggest = os.listdir(os.path.abspath(start_dir))
+        biggest = [File(f, os.path.getsize(f), os.path.abspath(os.path.join(f, os.pardir))) for f in
+                   os.listdir(os.path.abspath(start_dir))]
         if not consider_directories:
-            biggest = [f for f in biggest if not os.path.isdir(os.path.abspath(os.path.join(start_dir, f)))]
+            biggest = [f for f in biggest if not os.path.isdir(os.path.abspath(os.path.join(start_dir, f.name)))]
         if not consider_files:
-            biggest = [f for f in biggest if os.path.isdir(os.path.abspath(os.path.join(start_dir, f)))]
-    return sorted(biggest, key=lambda tup: tup[1], reverse=True)[:n]
+            biggest = [f for f in biggest if os.path.isdir(os.path.abspath(os.path.join(start_dir, f.name)))]
+    return sorted(biggest, key=lambda p: p.size, reverse=True)[:n]
 
 
 def advanced_search(path=None, size_range=None, date_range=None, extensions=None):
@@ -60,6 +74,29 @@ def advanced_search(path=None, size_range=None, date_range=None, extensions=None
     if size_range:
         files = [file for file in files if size_range[0] <= os.path.getsize(file) < size_range[1]]
     return [os.path.abspath(file) for file in files]
+
+
+def import_data(model, file_tree, root=None):
+    model.setRowCount(0)
+    if root is None:
+        root = model.invisibleRootItem()
+    seen = {}
+    queue = deque(file_tree)
+    while queue:
+        node = queue.popleft()
+        if node.level == 0:
+            parent = root
+        else:
+            parent_node_name = node.parent.name
+            if parent_node_name not in seen:
+                queue.append(node)
+                continue
+            parent = seen[parent_node_name]
+        node_name = node.name
+        parent.appendRow([
+            QStandardItem(node_name)
+        ])
+        seen[node_name] = parent.child(parent.rowCount() - 1)
 
 
 if __name__ == '__main__':
--- test/controller/disc_scanner.py
+++ test/controller/disc_scanner.py
@@ -4,17 +4,36 @@ from app.controller import disc_scanner
 
 
 def test_build_file_tree():
-    tree = disc_scanner.build_file_tree(os.path.abspath(os.path.join('..', 'test_dir')))
-    returned = tree.bfs()
+    returned = [(node.name, node.level) for node in disc_scanner.build_file_tree(os.path.abspath(os.path.join('..', 'test_dir')))]
+    # returned = tree.bfs()
     expected = {1: ['test_dir/'], 2: ['testfile', 'test_subdir/'], 3: ['testsubfile']}
-    assert returned == expected, \
-        'test_build_file_tree failed' \
-        'expected: {}' \
-        'returned: {}'.format(expected, returned)
+    print(returned)
+    # assert returned == expected, \
+    #     'test_build_file_tree failed' \
+    #     'expected: {}' \
+    #     'returned: {}'.format(expected, returned)
 
 
 def test_get_n_biggest():
-    print(disc_scanner.get_n_biggest(os.path.join('..', 'test_dir')))
+    print()
+    n_biggest = disc_scanner.get_n_biggest(os.path.join('..', 'test_dir'))
+    print(len(n_biggest))
+    print(n_biggest)
+
+
+def test_get_n_biggest_directories_only():
+    print()
+    n_biggest = disc_scanner.get_n_biggest(os.path.join('..', 'test_dir'), consider_files=False,
+                                           consider_directories=True)
+    print(len(n_biggest))
+    print(n_biggest)
+
+
+def test_get_n_biggest_all():
+    print()
+    n_biggest = disc_scanner.get_n_biggest(os.path.expanduser('~'), consider_directories=True)
+    print(len(n_biggest))
+    print(n_biggest)
 
 
 if __name__ == '__main__':
