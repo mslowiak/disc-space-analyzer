3c03b6f8fcd3d413f76f5eaf9378a6a72fb492ec
Konrad <konrad.korus0@gmail.com>
2019-01-30 19:58:22 +0100

    removed redundant code

--- app/controller/default_search_async.py
+++ app/controller/default_search_async.py
@@ -1,11 +1,7 @@
-import glob
-import os
-import time
 from threading import Thread
 
 from PyQt5.QtCore import pyqtSignal, QThread
 
-from model.file_tree import File
 from controller.disc_scanner import build_file_tree, get_n_biggest
 
 
@@ -40,26 +36,3 @@ class DefaultSearchThread(QThread):
         self.output['top_n_dirs'] = get_n_biggest(n=self.n, consider_files=False, consider_directories=True)
         self.sig1.emit(self.output)
 
-    def to_file(self, file_info_tuple):
-        """"
-        Mapper - properties to File class object
-        :param file_info_tuple: tuple with path and stats
-        """
-        f_path = file_info_tuple[0]
-        f_stats = file_info_tuple[1]
-        split_path = os.path.split(f_path)
-
-        f_dir_to_file = split_path[0]
-        split_file = os.path.splitext(split_path[1])
-        f_file_name = split_file[0]
-        f_file_ext = split_file[1]
-        f_file_size = f_stats.st_size
-        f_date_time = time.strftime("%D %H:%M", time.localtime(f_stats.st_mtime))
-
-        return File(
-            name=f_file_name,
-            location=f_dir_to_file,
-            extension=f_file_ext,
-            file_size=f_file_size,
-            creation_date=f_date_time
-        )
